fc76edda7094d28238edf3e1d863f6c68afa75b3
Isak Levinson <isak.levinson@gmail.com>
2019-05-06 12:22:13 +0300

    .gitignore

--- samples/nrf9160/at_client/src/main.c
+++ samples/nrf9160/at_client/src/main.c
@@ -14,16 +14,18 @@ void main(void)
 {
 	int err;
 
-	printf("The AT host sample started\n");
+	printf("The AT Ready.\n");
 
 	err = at_host_init(CONFIG_AT_HOST_UART, CONFIG_AT_HOST_TERMINATION);
 
 	if (err != 0) {
-		printf("ERROR: AT Host not initialized \r\n");
+		printf("ERROR: AT Host not initialized\r\n");
 		return;
 	}
 
 	while (1) {
 		at_host_process();
 	}
+
+        printf("ERROR: unexpected exit\r\n");
 }
